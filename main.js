5c3dd990c206dda526a8f71b340de68aa69088a7
Dane <wattsd1985@gmail.com>
2022-05-14 19:51:34 -0400

    Adding Repo

---
+++ main.js
@@ -0,0 +1,11 @@
+const d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
