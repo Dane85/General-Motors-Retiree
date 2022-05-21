7103b6bcab8d4fefdeaf6151a5e0636d01b8320b
Dane <wattsd1985@gmail.com>
2022-05-21 19:16:23 -0400

    Code Clean Up

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
